5866100612fc1c55de19dd0691ef67b81d0c3206
1Lacoste1 <macksafronov@yandex.ru>
2023-03-16 20:25:21 +0300

    Ввести с консоли имя файла. Найти байт или байты с минимальным количеством повторов. Вывести их на экран через пробел. Закрыть поток ввода-вывода.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,44 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        int a = 5;
-        System.out.println("Hello world!");
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        System.out.println(a);
+/*
+Самые редкие байты
+*/
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        byte[] buff = new byte[256];
+        int curr = Integer.MAX_VALUE;
+
+        try(FileInputStream fileInputStream = new FileInputStream(fileName)) {
+            while (fileInputStream.available() > 0) {
+                buff[fileInputStream.read()] += 1;
+            }
+        }
+
+        for(int count : buff) {
+            if(count > 0 && count < curr) {
+                curr = count;
+            }
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for(int i = 0; i < buff.length; i++) {
+            if(buff[i] == curr) {
+                arrayList.add(i);
+            }
+        }
+
+        for(Integer num : arrayList)
+            System.out.print(num + " ");
+
     }
 }
