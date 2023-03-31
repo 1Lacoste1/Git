b68ef6a633ff1a50a93a6dd25a95bfb272ed5fd8
1Lacoste1 <macksafronov@yandex.ru>
2023-03-31 16:41:59 +0300

    Добавил sout "Hello world"

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,46 @@
-//package com.javarush.task.task19.task1908;
-
+//package com.javarush.task.task19.task1909;
 
 import java.io.*;
 import java.util.ArrayList;
 
 /*
-Выделяем числа
+Замена знаков
 */
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String inputFileName;
-        String outputFileName;
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            inputFileName = reader.readLine();
-            outputFileName = reader.readLine();
-        }
-
-        ArrayList<String> fileContent = new ArrayList<>();
-        try (BufferedReader inputFileReader = new BufferedReader(new FileReader(inputFileName))) {
-            while (inputFileReader.ready()) {
-                fileContent.add(inputFileReader.readLine());
-            }
-        }
-
-        try (BufferedWriter outputFileWriter = new BufferedWriter(new FileWriter(outputFileName))) {
-            for (String line : fileContent) {
-                String[] splitLine = line.trim().split(" ");
-                for (String word : splitLine) {
-                    try {
-                        int num = Integer.parseInt(word);
-                        outputFileWriter.write(word + " ");
-                    } catch (Exception ignore) {
-                        /* NOP */
-                    }
-                }
-            }
-        }
-
-
+    public static void main(String[] s) {
+        A a = new C();
+        a.method2();
     }
-}
+
+    public static class A {
+        public void method1() {
+            System.out.println("A class, method1");
+        }
+
+        public void method2() {
+            System.out.println("A class, method2");
+        }
+    }
+
+    public static class B extends A {
+        public void method1() {
+            System.out.println("B class, method1");
+        }
+
+        public void method2() {
+            System.out.println("B class, method2");
+        }
+    }
+
+    public static class C extends B {
+        public void method1() {
+            System.out.println("C class, method1");
+        }
+
+        public void method2() {
+            System.out.println("C class, method2");
+            System.out.println("Hello world");
+        }
+    }
+    }
